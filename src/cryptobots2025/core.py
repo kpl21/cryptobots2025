5ddcdf298b6ae8d9d7f705701dabb1ef532fb590
hemangkaul <hemangkaul@gmail.com>
2025-02-22 23:38:21 -0500

    testing coinbase api

--- src/cryptobots2025/core.py
+++ src/cryptobots2025/core.py
@@ -3,29 +3,47 @@ from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 import os
 from dotenv import load_dotenv
+from json import dumps
+
+from coinbase.rest import RESTClient
 
 # Load environment variables from .env file
 load_dotenv()
 
+#coinbase stuff
+coinbase_api_key = os.getenv('COINBASE_API_KEY')
+coinbase_api_secret=os.getenv('COINBASE_API_SECRET')
+
+client = RESTClient(api_key=coinbase_api_key, api_secret=coinbase_api_secret)
+
+accounts = client.get_accounts()
+print(dumps(accounts.to_dict(), indent=2))
+
+order = client.market_order_buy(client_order_id="clientOrderId", product_id="BTC-USD", quote_size="1")
+print(dumps(order.to_dict(), indent=2))
+
+
+
+# coinmarketcap stuff
 # Get API key
-api_key = os.getenv('API_KEY')
+# api_key = os.getenv('API_KEY')
 
-url = 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest'
-parameters = {
-  'slug':'solana',
+# url = 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest'
+# parameters = {
+#   'slug':'solana',
 
-}
-headers = {
-  'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': api_key
-  }
+# }
+# headers = {
+#   'Accepts': 'application/json',
+#   'X-CMC_PRO_API_KEY': api_key
+#   }
 
-session = Session()
-session.headers.update(headers)
+# session = Session()
+# session.headers.update(headers)
 
-try:
-  response = session.get(url, params=parameters)
-  data = json.loads(response.text)
-  print(data['data']['5426']['quote'])
-except (ConnectionError, Timeout, TooManyRedirects) as e:
-  print(e)
+# try:
+#   response = session.get(url, params=parameters)
+#   data = json.loads(response.text)
+#   print(data['data']['5426']['quote'])
+# except (ConnectionError, Timeout, TooManyRedirects) as e:
+#   print(e)
