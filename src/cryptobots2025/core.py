7718bb75ff2abcc8f7938bfb2506e205e3201aca
hemangkaul <hemangkaul@gmail.com>
2025-02-23 17:33:00 -0500

    adding order class experiment

--- src/cryptobots2025/core.py
+++ src/cryptobots2025/core.py
@@ -4,6 +4,8 @@ import json
 import os
 from dotenv import load_dotenv
 from json import dumps
+import order
+import pandas as pd
 
 from coinbase.rest import RESTClient
 
@@ -16,38 +18,12 @@ coinbase_api_secret=os.getenv('COINBASE_API_SECRET')
 
 client = RESTClient(api_key=coinbase_api_key, api_secret=coinbase_api_secret)
 
-product = client.get_product_book("BTC-USD", limit=10)
+product = client.get_product_book("SOL-USD", limit=100)
 
 pricebook = product.pricebook
 
-bids = pricebook.bids
-asks = pricebook.asks
+bids = pricebook.bids # list of dictionaries
+asks = pricebook.asks # list of dictionaries
 print(bids)
 print(asks)
 
-# with open("../../data/product.json", 'w') as file:
-    
-    
-# coinmarketcap stuff
-# Get API key
-# api_key = os.getenv('API_KEY')
-
-# url = 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest'
-# parameters = {
-#   'slug':'solana',
-
-# }
-# headers = {
-#   'Accepts': 'application/json',
-#   'X-CMC_PRO_API_KEY': api_key
-#   }
-
-# session = Session()
-# session.headers.update(headers)
-
-# try:
-#   response = session.get(url, params=parameters)
-#   data = json.loads(response.text)
-#   print(data['data']['5426']['quote'])
-# except (ConnectionError, Timeout, TooManyRedirects) as e:
-#   print(e)
